3f5fb84effc03aea4d589a2b49990ad65cb343b0
Tufem Gift Items <Laughteriremide@gmail.com>
2025-02-17 16:19:37 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from "./Components/Nav";
 import Menu from "./Components/Menu";
 import Card from "./Components/Card";
 import Card2 from "./Components/Card2";
-import Reserve from "./Components/REserve";
+import Reserve from "./Components/Reserve";
 import Footer from "./Components/Footer";
 
 function App() {
